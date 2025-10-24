0ea1cb5df06eb9314f4e012ab3cbe42ce7fa69be
Taewoong Heo <chamjoeun0111@gmail.com>
2025-10-24 23:20:15 +0900

    feat: add Adominition block component

---
+++ src/components/Admonition.tsx
@@ -0,0 +1,28 @@
+interface AdmonitionProps {
+  title: string;
+  content: string;
+}
+
+const Admonition = ({ title, content }: AdmonitionProps) => {
+  return (
+    <div className="my-6 rounded-lg border-2 border-amber-400 bg-amber-50 p-4 not-prose">
+      <div className="flex items-center gap-3">
+        <div className="text-amber-600">
+          <svg className="h-5 w-5" fill="currentColor" viewBox="0 0 20 20">
+            <path
+              fillRule="evenodd"
+              d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z"
+              clipRule="evenodd"
+            />
+          </svg>
+        </div>
+        <div className="text-amber-800">
+          <p className="font-semibold">{title}</p>
+          <p className="text-sm">{content}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Admonition;
