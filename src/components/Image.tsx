b1867fe24cd48a8ee79b7807fdcbd54e65eadb76
Taewoong Heo <chamjoeun0111@gmail.com>
2025-10-24 23:28:23 +0900

    fix: resolve image paths with base URL configuration

--- src/components/Image.tsx
+++ src/components/Image.tsx
@@ -1,10 +1,18 @@
+import { baseURL } from "@/lib/url";
+
 interface ImageProps {
   src: string;
   alt: string;
 }
 
 const Image = ({ src, alt }: ImageProps) => {
-  return <img src={src} alt={alt} className="w-full sm:w-3/4 md:w-1.5/2" />;
+  return (
+    <img
+      src={`${baseURL}${src}`}
+      alt={alt}
+      className="w-full sm:w-3/4 md:w-1.5/2"
+    />
+  );
 };
 
 export default Image;
