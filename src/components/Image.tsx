a148e48604b8d0637dcec813e98e622d4ce22dc4
Taewoong Heo <chamjoeun0111@gmail.com>
2025-10-24 23:20:26 +0900

    feat: add Image component

---
+++ src/components/Image.tsx
@@ -0,0 +1,10 @@
+interface ImageProps {
+  src: string;
+  alt: string;
+}
+
+const Image = ({ src, alt }: ImageProps) => {
+  return <img src={src} alt={alt} className="w-full sm:w-3/4 md:w-1.5/2" />;
+};
+
+export default Image;
